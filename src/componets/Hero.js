907cd79306dbb5aa6869519a3ca0fcd52f8e5c9f
mohamed <mohamedhadd95@gmail.com>
2023-04-08 15:51:52 +0100

    first commit

---
+++ src/componets/Hero.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='hero1'>
+    
+<div className="p-4-1 shadow-4 rounded-3">
+  <h2>Hello world!</h2>
+  <p>
+    This is a simple hero unit, a simple Hero-style component for calling extra
+    attention to featured content or information.
+  </p>
+
+  <hr className="my-4" />
+
+  <p>
+    It uses utility classes for typography and spacing to space content out within the
+    larger container.
+  </p>
+  <button type="button" className="btn btn-primary">
+    Learn more
+  </button>
+</div>
+    </div>
+  )
+}
+
+export default Hero
